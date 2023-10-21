97ccd6c25a11c45cee5c73a827a5cb480e807f0f
dodir <mokronosovmw@gmail.com>
2023-10-22 04:22:34 +0500

    lesson 11: frontend and backend linking: CORS and middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
@@ -26,6 +27,24 @@ app.include_router(
 
 app.include_router(router_operation)
 app.include_router(router_task)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
 
 
 @app.on_event("startup")
